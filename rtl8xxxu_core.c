0fb2fa3870f557a29886d2bf0f1d0b270b2ef018
kimocoder <christian@aircrack-ng.org>
2020-12-28 21:10:45 +0100

    Fix some compiler issues

--- rtl8xxxu_core.c
+++ rtl8xxxu_core.c
@@ -5305,7 +5305,7 @@ static void rtl8723bu_handle_c2h(struct rtl8xxxu_priv *priv,
 	case C2H_8723B_RA_REPORT:
 		dev_dbg(dev,
 			"C2H RA RPT: rate %02x, unk %i, macid %02x, noise %i\n",
-			c2h->ra_report.rate, c2h->ra_report.dummy0_0,
+			c2h->ra_report.rate, c2h->ra_report.dummy2_0,
 			c2h->ra_report.macid, c2h->ra_report.noisy_state);
 		break;
 	default:
